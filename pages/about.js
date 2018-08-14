498558160b9604cc144cccbf02c83cd7f2439841
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-14 16:12:33 +0100

    remove link from about page

--- pages/about.js
+++ pages/about.js
@@ -2,10 +2,6 @@ import Link from 'next/link'
 
 export default () => (
     <div>
-        <Link href="/"><a><li style={{ display: 'flex', margin: 40, fontSize: 20 }} >Home</li></a></Link>
-        <p>This is the about page</p>
-        <Link href="/">
-            <button>Go to Home Page</button>
-        </Link>
+         <p>This is the about page</p>
     </div>
 )
