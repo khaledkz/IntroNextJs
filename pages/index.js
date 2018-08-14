483363def4d0b497ee610f088f66bb981cae6e83
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-14 16:12:47 +0100

    remove link from home page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Link from 'next/link'
 
 const Index = () => (
     <div>
-        <Link href="/about" ><a><li style={{ display: 'flex', margin: 40 ,fontSize:20}}>About</li></a></Link>
         <p>Hello Next.js</p>
     </div>
 )
