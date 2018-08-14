8f3c14c60e52b6c892f169f36713ad8755127167
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-14 15:42:30 +0100

    update index page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Index = () => (
     <div>
-        <Link href="/about"><a><li style={{ display: 'flex', margin: 40 }}>About</li></a></Link>
+        <Link href="/about" ><a><li style={{ display: 'flex', margin: 40 ,fontSize:20}}>About</li></a></Link>
         <p>Hello Next.js</p>
     </div>
 )
