f19330cee2fcc16997783eb1e16129eaba1af3e6
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-14 16:27:41 +0100

    remvoe header from home

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,6 @@
-import Header from '../components/Header'
-
+ 
 export default () => (
     <div>
-         <Header/>
-         <p>This is the about page</p>
+          <p>This is the about page</p>
     </div>
 )
