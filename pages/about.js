f5860c7942ba0e5f1ad1183b208440c8d8ad4163
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-14 15:44:26 +0100

    navigate using button with link

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,10 @@ import Link from 'next/link'
 
 export default () => (
     <div>
-        <Link href="/"><a><li style={{ display: 'flex', margin: 40 ,fontSize:20}} >Home</li></a></Link>
+        <Link href="/"><a><li style={{ display: 'flex', margin: 40, fontSize: 20 }} >Home</li></a></Link>
         <p>This is the about page</p>
+        <Link href="/">
+            <button>Go to Home Page</button>
+        </Link>
     </div>
 )
