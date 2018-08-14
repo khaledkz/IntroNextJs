936b434a97d0933bd2f943574afc0574c9b7776a
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-14 15:42:24 +0100

    update about page

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default () => (
     <div>
-        <Link href="/"><a><li style={{ display: 'flex', margin: 40 }}>Home</li></a></Link>
+        <Link href="/"><a><li style={{ display: 'flex', margin: 40 ,fontSize:20}} >Home</li></a></Link>
         <p>This is the about page</p>
     </div>
 )
