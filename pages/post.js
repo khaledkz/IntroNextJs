5fa36bc6bc49d357b84b6a31b3ca531dd584e1e0
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-15 14:56:16 +0100

    style post using jsx css

--- pages/post.js
+++ pages/post.js
@@ -3,13 +3,33 @@ import fetch from 'isomorphic-unfetch'
 
 const Post =  (props) => (
     <Layout>
+        <div>
        <h1>{props.show.name}</h1>
        <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
-       <img src={props.show.image.medium}/>
+       <img src={props.show.image.medium}/> </div>
+       <style jsx>
+      
+        {`
+          div{
+            width:60%;
+            border:solid 1px silver;
+            margin:40px 20% 10px 20%;
+            display:flex;
+            flex-direction:column;
+            justify-content:center; 
+            align-items:center;
+            min-height:600px;
+           }
+          p{
+              width:90%
+          }
+        `}
+       </style>
     </Layout>
 )
 
 Post.getInitialProps = async function (context) {
+  console.log('print only in server')
   const { id } = context.query
   const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
   const show = await res.json()
