a126cb0481df38b56095f2c5ada93107d1d25e79
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-23 14:23:02 +0100

    comment code

--- test.js
+++ test.js
@@ -23,15 +23,15 @@
 //ES6: Use Destructuring Assignment with the Rest Operator to Reassign Array Elements
 
 
-const source = [1,2,3,4,5,6,7,8,9,10];
-function removeFirstTwo(list) {
-  "use strict";
-  // change code below this line
-  let arr,a,b;
-  [a,b,...arr]=source
+// const source = [1,2,3,4,5,6,7,8,9,10];
+// function removeFirstTwo(list) {
+//   "use strict";
+//   // change code below this line
+//   let arr,a,b;
+//   [a,b,...arr]=source
  
-  return arr;
-}
-const arr = removeFirstTwo(source);
-console.log(arr); // should be [3,4,5,6,7,8,9,10]
-console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
+//   return arr;
+// }
+// const arr = removeFirstTwo(source);
+// console.log(arr); // should be [3,4,5,6,7,8,9,10]
+// console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
