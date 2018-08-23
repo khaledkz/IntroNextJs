f45dc3aedcc1e5478f34a47cd1d607973a12b8c8
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-23 14:20:25 +0100

    comment code

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
-let a ={a:{b:{c:1}}}
+// let a ={a:{b:{c:1}}}
 
-let b = a?.a;
-console.log(b)
+// let b = a?.a;
+// console.log(b)
 
 
-var user = { name: 'Joe' }
-var zip = user?.address?.zip;
-console.log(zip)
+// var user = { name: 'Joe' }
+// var zip = user?.address?.zip;
+// console.log(zip)
 
-var street = user.address && user.address.street;
+// var street = user.address && user.address.street;
 
  
