08e84c049ed38ea633d664147a0fb942d5d53c0e
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-23 14:20:01 +0100

    optional chaining

---
+++ test.js
@@ -0,0 +1,13 @@
+let a ={a:{b:{c:1}}}
+
+let b = a?.a;
+console.log(b)
+
+
+var user = { name: 'Joe' }
+var zip = user?.address?.zip;
+console.log(zip)
+
+var street = user.address && user.address.street;
+
+ 
